d4b6f1eeb871a755ffedb07c7bfb11f9148f50b7
Hieubkav <tranmanhhieu10@gmail.com>
2025-03-01 13:53:40 +0700

    first setup

---
+++ database/seeders/FirstSeed.php
@@ -0,0 +1,26 @@
+<?php
+
+    namespace Database\Seeders;
+
+    use App\Models\Carousel;
+    use App\Models\CatPost;
+    use App\Models\Doctor;
+    use App\Models\Page;
+    use App\Models\Post;
+    use App\Models\Service;
+    use App\Models\Setting;
+    use App\Models\User;
+    use Illuminate\Database\Seeder;
+
+    class FirstSeed extends Seeder {
+        /**
+         * Run the database seeds.
+         *
+         * @return void
+         */
+        public function run(): void {
+            
+
+
+        }
+    }
